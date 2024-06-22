38bed1fcdc9905eb5f7ae30c962ce901f88592f7
David Kwong <20544dk@gmail.com>
2024-06-22 17:21:16 +1000

    Remove confusing constants

--- src/consts.rs
+++ src/consts.rs
@@ -1,9 +1,3 @@
-pub const ADDR_SIZE: usize = 4;
 pub const PAGE_SIZE: usize = 4096;
-
-pub const KEY_HEADER_SIZE: usize = 4;
-pub const VALUE_HEADER_SIZE: usize = 4;
-
-pub const MAX_PAGE_CACHED_COUNT: usize = 10;
 
 
