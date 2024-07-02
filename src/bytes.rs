3b89427882b12322a3b3246a832d29103e70fe50
David Kwong <20544dk@gmail.com>
2024-07-02 17:06:00 +1000

    Simplify trait so that many more people can consume it

--- src/bytes.rs
+++ src/bytes.rs
@@ -6,10 +6,7 @@ pub trait IntoBytes {
     fn into_bytes(self) -> Vec<u8>;
 }
 
-pub trait ParseFromBytes<'a, T>: Sized
-where
-    T: Iterator<Item = &'a u8>,
-{
+pub trait ParseFromBytes<T>: Sized {
     type Error;
     type Metadata;
     fn from_bytes(bytes: T, metadata: Self::Metadata) -> Result<(Self, T), Self::Error>;
